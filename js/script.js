bf398ce20d302f593b093b588ebb20655bbd33eb
David Forrester <david.o.forrester@gmail.com>
2023-06-15 20:25:21 +1000

    dropdown buttons and divs added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-// Variables
+const menuButtons = document.getElementsByClassName("menu-button");
 
-
-// Code body
-
-
-// Functions
-
+for (menuButton of menuButtons) {
+  menuButton.addEventListener("click", () => {
+    const menu = document.getElementById("menu");
+    if (menu.hidden == true) {
+      menu.hidden = false;
+    } else {
+      menu.hidden = true;
+    }
+  });
+}
