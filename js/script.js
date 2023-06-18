7f0c42e73a5254218aab3f9c51a76cd7f3e7cece
David Forrester <david.o.forrester@gmail.com>
2023-06-18 20:36:43 +1000

    Mobile dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let clickState = "closed";
+// Dropdown
+/*let clickState = "closed";
 
 const menuButton = document.getElementById("menu-button");
 const menu = document.getElementById("menu")
@@ -18,5 +19,30 @@ menuButton.addEventListener("mouseover", () => {
 menuButton.addEventListener("mouseout", () => {
   if (clickState == "closed") {
     menu.classList.remove('show')
+  }
+});*/
+
+
+// Mobile Dropdown
+let mobileClickState = "closed";
+
+const mobileMenuButton = document.getElementById("mobile-menu-button");
+const mobileMenu = document.getElementById("mobile-menu-options")
+
+mobileMenuButton.addEventListener("click", () => {
+  if (mobileClickState == "closed") {
+    mobileClickState = "open";
+  } else {
+    mobileClickState = "closed";
+  }
+});
+
+mobileMenuButton.addEventListener("mouseover", () => {
+    mobileMenu.classList.add('show')
+});
+
+mobileMenuButton.addEventListener("mouseout", () => {
+  if (mobileClickState == "closed") {
+    mobileMenu.classList.remove('show')
   }
 });
